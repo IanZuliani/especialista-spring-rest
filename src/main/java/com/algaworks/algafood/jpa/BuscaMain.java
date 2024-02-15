6b3a98dcfd281cb417ccb5cde4f6c9b40c8a11db
ianzuliani <rubenszuliani@gmail.com>
2024-02-15 22:43:45 +0000

    3.10. Buscando um objeto pelo id no banco de dados

---
+++ src/main/java/com/algaworks/algafood/jpa/BuscaMain.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.jpa;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class BuscaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        //Consultar Cozinha
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinhas = cadastroCozinha.buscar(1L);
+
+        System.out.println("cozinhas = " + cozinhas.getNome());
+
+
+
+    }
+}
