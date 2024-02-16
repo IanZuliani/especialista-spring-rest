54c22c6c59a0afb8f85021f40dd5abdbb82fad46
ianzuliani <rubenszuliani@gmail.com>
2024-02-16 23:40:23 +0000

    3.19. Propriedade nullable de @Column e @JoinColumn

--- src/main/java/com/algaworks/algafood/domain/model/Estado.java
+++ src/main/java/com/algaworks/algafood/domain/model/Estado.java
@@ -4,26 +4,19 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
-import java.math.BigDecimal;
-import java.util.Objects;
+
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Restaurante {
+public class Estado {
 
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
-
-    @Column(name = "taxa_frete", nullable = false)
-    private BigDecimal taxaFrete;
-
-    @ManyToOne
-    @JoinColumn(name = "cozinha_id", nullable = false)
-    private Cozinha cozinha;
 
 }
