80722179184e1fc7e0fc8b7f02d09399ffd27da8
ianzuliani <rubenszuliani@gmail.com>
2024-02-16 17:40:06 +0000

    3.15 Adicionando Lombok e desafio Lombok

--- src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -13,30 +13,29 @@ import java.util.List;
 @Component
 public class CozinhaRepositoryImpl implements CozinhaRepository {
 
-    @PersistenceContext //a notacao do JPA para instanciarmos um EntityManager
+    @PersistenceContext
     private EntityManager manager;
 
     @Override
-    public List<Cozinha> listar(){
-        TypedQuery<Cozinha> query =  manager.createQuery("from Cozinha", Cozinha.class);
-
-        return query.getResultList();
+    public List<Cozinha> listar() {
+        return manager.createQuery("from Cozinha", Cozinha.class)
+                .getResultList();
     }
 
     @Override
-    public Cozinha buscar(Long id){
+    public Cozinha buscar(Long id) {
         return manager.find(Cozinha.class, id);
     }
 
-    @Override
     @Transactional
-    public Cozinha salvar(Cozinha cozinha){
+    @Override
+    public Cozinha salvar(Cozinha cozinha) {
         return manager.merge(cozinha);
     }
 
-    @Override
     @Transactional
-    public void remover(Cozinha cozinha){
+    @Override
+    public void remover(Cozinha cozinha) {
         cozinha = buscar(cozinha.getId());
         manager.remove(cozinha);
     }
