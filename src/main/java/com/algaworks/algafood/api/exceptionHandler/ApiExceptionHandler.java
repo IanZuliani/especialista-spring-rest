763bf86631bb3c172e8ad9c69c855238dd0e76c3
ianzuliani <rubenszuliani@gmail.com>
2024-03-13 22:33:27 +0000

    8.15. Criando um exception handler global com ResponseEntityExceptionHandler

--- src/main/java/com/algaworks/algafood/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionHandler/ApiExceptionHandler.java
@@ -3,16 +3,18 @@ package com.algaworks.algafood.api.exceptionHandler;
 import com.algaworks.algafood.domain.exception.EndidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.NegocioExceptional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EndidadeNaoEncontradaException.class)
     public ResponseEntity<?> tratarEstadoNaoEncontradoException(EndidadeNaoEncontradaException e){
         var problema = Problema.builder()
@@ -40,16 +42,6 @@ public class ApiExceptionHandler {
                 .mensagem(e.getMessage()).build();
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(problema);
-    }
-
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException(){
-        var problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem("O tipo de media nao e aceito").build();
-
-        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
                 .body(problema);
     }
 
