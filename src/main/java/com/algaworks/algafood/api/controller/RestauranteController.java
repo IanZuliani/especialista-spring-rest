c2f49a4776c19bfc89f8bf3a26c0f4835019c741
ianzuliani <rubenszuliani@gmail.com>
2024-03-17 18:01:19 +0000

    8.24. Lançando exception de desserialização na atualização parcial (PATCH)

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -7,16 +7,24 @@ import com.algaworks.algafood.domain.exception.NegocioExceptional;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.IgnoredPropertyException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
@@ -71,27 +79,39 @@ public class RestauranteController {
 
     @PatchMapping("/{restauranteId}")
     public Restaurante atualizarParcial(@PathVariable Long restauranteId,
-                                              @RequestBody Map<String, Object> campos) {
+                                        @RequestBody Map<String, Object> campos,
+                                        HttpServletRequest request) {
         Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 
-        merge(campos, restauranteAtual);
+        merge(campos, restauranteAtual, request);
 
         return editar(restauranteId, restauranteAtual);
     }
 
-    private static void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+    private static void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
+        var server = new ServletServerHttpRequest(request);
 
-        dadosOrigem.forEach((nome, valor)->{
-            Field field = ReflectionUtils.findField(Restaurante.class, nome);
-            field.setAccessible(true);
+        try {
 
-            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+            Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
 
-            //System.out.println(nome +" = "+ valor);
-            ReflectionUtils.setField(field, restauranteDestino, novoValor);
+            dadosOrigem.forEach((nome, valor)->{
+                Field field = ReflectionUtils.findField(Restaurante.class, nome);
+                field.setAccessible(true);
 
-        });
+                Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+                //System.out.println(nome +" = "+ valor);
+                ReflectionUtils.setField(field, restauranteDestino, novoValor);
+
+            });
+        }catch (IllegalArgumentException e){
+
+            Throwable rootCause = ExceptionUtils.getRootCause(e);
+            throw new HttpMessageNotReadableException(e.getMessage(), rootCause, server);
+        }
     }
 }
