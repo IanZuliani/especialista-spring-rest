0690c1e9357e687755640832dcca139dac50e443
ianzuliani <rubenszuliani@gmail.com>
2024-04-14 21:36:43 +0100

    12.14. Desafio: Implementando os endpoints de abertura e fechamento de restaurantes

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -162,4 +162,16 @@ public class RestauranteController {
     public void inativar(@PathVariable Long id){
         cadastroRestaurante.inativar(id);
     }
+
+    @PutMapping("/{restauranteId}/abertura")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void abrir(@PathVariable Long restauranteId) {
+        cadastroRestaurante.abrir(restauranteId);
+    }
+
+    @PutMapping("/{restauranteId}/fechamento")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void fechar(@PathVariable Long restauranteId) {
+        cadastroRestaurante.fechar(restauranteId);
+    }
 }
