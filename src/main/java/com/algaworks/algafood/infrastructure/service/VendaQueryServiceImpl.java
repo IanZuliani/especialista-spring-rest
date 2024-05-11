696b9329e202386485a53f2ffe064677e91f8a9d
ianzuliani <rubenszuliani@gmail.com>
2024-05-11 22:16:44 +0100

    Criando os filtros status = CONFIRMADO, ENTREGUE

--- src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.infrastructure.service;
 
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.StatusPedido;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,9 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -63,6 +66,29 @@ public class VendaQueryServiceImpl implements VendaQueryService {
                 functioDateDataCriacao,
                 builder.count(root.get("id")),
                 builder.sum(root.get("valorTotal")));
+
+        /**
+         * Criando os filtros
+         * status = CONFIRMADO, ENTREGUE
+         */
+
+        var predicates = new ArrayList<Predicate>();
+
+        if(filtro.getRestauranteId() != null){
+            predicates.add(builder.equal(root.get("id"), filtro.getRestauranteId()));
+        }
+        if (filtro.getDataCriacaoInicio() != null){
+            predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+        }
+        if (filtro.getDataCriacaoFim() != null){
+            predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+        }
+
+        predicates.add(root.get("status").in(
+                StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+
+
+        query.where(predicates.toArray(new Predicate[0]));
 
         /**
          * Vamos para proximo passo que e fazer o select no banco com o que queremos selecionar, que no caso e o retorno
