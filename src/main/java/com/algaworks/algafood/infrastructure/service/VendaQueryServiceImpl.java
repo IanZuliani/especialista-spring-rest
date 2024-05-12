3c00ebe53429d76cd7c9c5de6b81e2dc7bfc8f23
ianzuliani <rubenszuliani@gmail.com>
2024-05-12 19:20:58 +0100

    13.16. Tratando time offset na agregação de vendas diárias por data

--- src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -23,7 +23,7 @@ public class VendaQueryServiceImpl implements VendaQueryService {
     private EntityManager manager;//a partir do EntityManager conseguimos dar um criteriaBuilder
 
     @Override
-    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
+    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro,String timeOffset) {
 
         /**
          * Obtemos uma instancia de CriteriaBuilder que e utilizada para criar
@@ -41,6 +41,22 @@ public class VendaQueryServiceImpl implements VendaQueryService {
          */
         var root = query.from(Pedido.class);
 
+
+        /**
+         * Funcao para alterar a data para o horario de Brasilia Brazil com offset +3
+         * convert_tz -> nome da funcao no Mysql
+         * root.get("dataCriacao") -> Data Criacao do pedido
+         * Date.class -> tipo esperado
+         * builder.literal("+00:00") -> Primeiro argumento e um Literal de string, Pos espera uma Expression
+         * Nossa Api tem que ser Flexivel, por isso nao vamos colocar o codigo do timezone de Brasilia,
+         * Tera qye ser passado Pelo Controllador, que recebe como argumento pelo Responsavel Pela API
+         *
+         */
+        var functionConvertTzDataCriacao = builder.function(
+                "convert_tz",Date.class, root.get("dataCriacao"),  builder.literal("+00:00"), builder.literal(timeOffset));
+
+
+
         /**
          * O resultado da pesquisa ele vai ser usado para chamar o construtor de uma classe
          * Qual classe
@@ -50,7 +66,7 @@ public class VendaQueryServiceImpl implements VendaQueryService {
          * 1  - data_criacao -> ai ja e mas complicado visto que o nao tem a funcao date() do sql
          * Vamos ter que criar uma funcao separada para poder utilizar ela
          * var functioDateDataCroacao = builder.function()
-         *  1 - Parametro e o nome da funcao no banco "date"
+         *  1 - Parametro e o nome da funcao no bÀ medida que a adoção da extensão cresce, poderemos adicionar suporte para WebStorm no futuro.anco "date"
          *  2 - Parametro quando executamos essa funcao que tipo de retorno queremos - LocalDate
          *  3 - Quais sao os argumentos a funcao, Podemos ter mas de 1 - nesse caso e dataCriacao
          * 2 - totalVendas -> Faz um count no id, utilizando builder.count - root e o pedido
@@ -60,7 +76,8 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 
         var functioDateDataCriacao = builder.function("date",
                 Date.class,
-                root.get("dataCriacao"));
+                functionConvertTzDataCriacao);
+
 
         var selection = builder.construct(VendaDiaria.class,
                 functioDateDataCriacao,
@@ -73,6 +90,8 @@ public class VendaQueryServiceImpl implements VendaQueryService {
          */
 
         var predicates = new ArrayList<Predicate>();
+
+
 
         if(filtro.getRestauranteId() != null){
             predicates.add(builder.equal(root.get("id"), filtro.getRestauranteId()));
