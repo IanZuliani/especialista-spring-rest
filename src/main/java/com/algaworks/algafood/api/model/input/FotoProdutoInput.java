90f20963e4dd1a12ce945ae4e2b843ae61f376f0
ianzuliani <rubenszuliani@gmail.com>
2024-05-18 16:39:23 +0100

    14.4. Desafio: Validando o content type do arquivo Utilizar a anotacao @FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})

--- src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
@@ -1,8 +1,10 @@
 package com.algaworks.algafood.api.model.input;
 
+import com.algaworks.algafood.core.validation.FileContentType;
 import com.algaworks.algafood.core.validation.FileSize;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
@@ -18,6 +20,7 @@ public class FotoProdutoInput {
 
     @NotNull
     @FileSize(max = "1MG")
+    @FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
     private MultipartFile arquivo;
     @NotBlank
     private String descricao;
---
+++ src/main/java/com/algaworks/algafood/core/validation/FileContentTypeValidator.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.core.validation;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileContentTypeValidator implements ConstraintValidator<FileContentType, MultipartFile> {
+
+
+    private List<String> allowedContentTypes;
+
+    @Override
+    public void initialize(FileContentType constraintAnnotation) {
+        this.allowedContentTypes = Arrays.asList(constraintAnnotation.allowed());
+    }
+
+    @Override
+    public boolean isValid(MultipartFile multipartFile, ConstraintValidatorContext constraintValidatorContext) {
+        return multipartFile == null
+                || this.allowedContentTypes.contains(multipartFile.getContentType());
+
+
+    }
+}
