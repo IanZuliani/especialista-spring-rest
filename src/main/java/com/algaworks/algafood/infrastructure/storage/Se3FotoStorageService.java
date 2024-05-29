801ace4937352c5b96a7c9c41c1f17c7daae40ec
ianzuliani <rubenszuliani@gmail.com>
2024-05-29 21:50:12 +0100

    14.24. Desafio: Implementando a exclusão de objetos do bucket da Amazon S3

--- src/main/java/com/algaworks/algafood/infrastructure/storage/Se3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/storage/Se3FotoStorageService.java
@@ -4,6 +4,7 @@ import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -78,6 +79,15 @@ public class Se3FotoStorageService implements FotoStorageService {
 
     @Override
     public void remover(String nomeArquivo) {
+
+        /**
+         * Deletar um arquivo na amazon
+         * Primeiro Eu crio o caminho do arquivo que quero deletar
+         */
+        String caminhoArquivo = getCaminhoArquivo(nomeArquivo);
+
+        var deleteObjectRequest = new DeleteObjectRequest(storageProperties.getS3().getBucket(), caminhoArquivo);
+        amazonS3.deleteObject(deleteObjectRequest);
 
     }
 }
