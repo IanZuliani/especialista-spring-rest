40d5a4f3981b157f17fbdbbf80d2e6608f8b03a3
ianzuliani <rubenszuliani@gmail.com>
2024-07-02 23:05:31 +0100

    18.5. Selecionando os endpoints da API para gerar a documentação

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,7 +1,9 @@
 package com.algaworks.algafood.core.openapi;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -20,7 +22,9 @@ public class SpringFoxConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                .apis(RequestHandlerSelectors.any())//pode colocar tudo o que achar na api
+                .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
+                .paths(PathSelectors.any())
+                //.paths(PathSelectors.ant("/restaurantes/*"))
                 .build();
     }
 }
