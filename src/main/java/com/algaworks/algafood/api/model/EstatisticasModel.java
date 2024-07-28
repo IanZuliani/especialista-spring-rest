0e906f917259b1cc4762e063bcd7b7a17507978d
ianzuliani <rubenszuliani@gmail.com>
2024-07-28 22:48:34 +0100

    19.37. Desafio: implementando endpoint com links de recursos de estatísticas

--- src/main/java/com/algaworks/algafood/api/model/EstatisticasModel.java
+++ src/main/java/com/algaworks/algafood/api/model/EstatisticasModel.java
@@ -2,6 +2,6 @@ package com.algaworks.algafood.api.model;
 
 import org.springframework.hateoas.RepresentationModel;
 
-public static class EstatisticasModel extends RepresentationModel<EstatisticasModel> {
+public class EstatisticasModel extends RepresentationModel<EstatisticasModel> {
 }
 
